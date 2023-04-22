254092ad62898aed9762d6df8d6f86a626abfeec
Arpana <arp2ctaula@gmail.com>
2023-04-22 15:29:10 +0545

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Login from './pages/Login'
+
+function App() {
+
+  return (
+    <React.Fragment>
+      <Login></Login>
+    </React.Fragment>
+  )
+}
+
+export default App
---
+++ src/pages/Login.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <React.Fragment>Hi I am login page</React.Fragment>
+  )
+}
+
+export default Login;
