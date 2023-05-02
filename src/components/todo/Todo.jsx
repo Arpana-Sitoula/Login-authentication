e02064714da111fca1ce80a39738df516f0d8b7c
Nirajan <nirajan@pop-os.localdomain>
2023-05-02 21:50:28 +0545

    solved rendering issue

--- src/components/todo/Todo.jsx
+++ src/components/todo/Todo.jsx
@@ -1,24 +1,21 @@
-import React, { useEffect, useState } from 'react'
-import ResourceList from './ResourceList'
-import Button from '../resuable/Button';
+import React, { useEffect, useState } from "react";
+import ResourceList from "./ResourceList";
+import Button from "../resuable/Button";
 
 const Todo = () => {
-  const [resourceType, setResourceType] = useState('')
+  const [resourceType, setResourceType] = useState("");
   useEffect(() => {
-    console.log("render")
-  }, [resourceType])
+    console.log("render");
+  }, [resourceType]);
   return (
     <React.Fragment>
       <div>
-        <Button
-          title="Todo" onClick={() => setResourceType("todos")
-          } />
-        <Button
-          title="Post" onClick={() => setResourceType("posts")} />
+        <Button title="Todo" onClick={() => setResourceType("todos")} />
+        <Button title="Post" onClick={() => setResourceType("posts")} />
       </div>
       <ResourceList resourceType={resourceType} />
     </React.Fragment>
-  )
-}
+  );
+};
 
-export default Todo
+export default Todo;
